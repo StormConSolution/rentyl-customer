c3447a75c7d4f64434edacf06a538ae62e9ea8df
janel <janell@redskytech.io>
2021-02-15 16:19:32 -0700

    StarRating Component

---
+++ src/components/starRating/StarRating.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import './StarRating.scss';
+import { Box } from '@bit/redsky.framework.rs.996';
+
+interface starRatingProps {
+	size: 'small16px' | 'medium24px' | 'large32px';
+	rating: 0 | 0.5 | 1 | 1.5 | 2 | 2.5 | 3 | 3.5 | 4 | 4.5 | 5;
+}
+
+const StarRating: React.FC<starRatingProps> = (props) => {
+	function createStarRating() {
+		let htmlElement: JSX.Element[] = [];
+		for (let i = 0; i < 5; i++) {
+			if (i < props.rating) {
+				if (i + 0.5 === props.rating) htmlElement.push(halfStar());
+				else htmlElement.push(wholeStar());
+			} else {
+				htmlElement.push(emptyStar());
+			}
+		}
+		return htmlElement;
+	}
+
+	function getBackgroundSizeClassName() {
+		let backgroundClassName;
+		if (props.size === 'small16px') backgroundClassName = 'smallStarBackground';
+		else if (props.size === 'medium24px') backgroundClassName = 'medStarBackground';
+		else backgroundClassName = 'largeStarBackground';
+		return backgroundClassName;
+	}
+
+	function getBorderSizeClassName() {
+		let borderClassName;
+		if (props.size === 'small16px') borderClassName = 'smallStarBorder';
+		else if (props.size === 'medium24px') borderClassName = 'medStarBorder';
+		else borderClassName = 'largeStarBorder';
+		return borderClassName;
+	}
+
+	function wholeStar() {
+		const borderClassName = getBorderSizeClassName();
+		return (
+			<Box className={'starRating'}>
+				<Box className={`starRatingBorder ${borderClassName}`} />
+			</Box>
+		);
+	}
+
+	function halfStar() {
+		const borderClassName = getBorderSizeClassName();
+		const backgroundClassName = getBackgroundSizeClassName();
+		return (
+			<Box className={'starRating'}>
+				<Box className={`starRatingBorder ${borderClassName}`} />
+				<Box className={`starRatingBackground ${backgroundClassName}`} />
+				<Box className={`halfStar ${borderClassName}`} />
+			</Box>
+		);
+	}
+
+	function emptyStar() {
+		const borderClassName = getBorderSizeClassName();
+		const backgroundClassName = getBackgroundSizeClassName();
+		return (
+			<Box className={'starRating'}>
+				<Box className={`starRatingBorder ${borderClassName}`} />
+				<Box className={`starRatingBackground ${backgroundClassName}`} />
+			</Box>
+		);
+	}
+
+	return <Box className={'rsStarRating'}>{createStarRating()}</Box>;
+};
+export default StarRating;
--- src/pages/signupPage/SignupPage.tsx
+++ src/pages/signupPage/SignupPage.tsx
@@ -1,30 +1,166 @@
-import React from 'react';
+import * as React from 'react';
+import './SignUpPage.scss';
 import { Page } from '@bit/redsky.framework.rs.996';
-import './SignupPage.scss';
+import Footer from '../../components/footer/Footer';
+import { FooterLinkTestData } from '../../components/footer/FooterLinks';
 import Box from '../../components/box/Box';
-import useWindowResizeChange from '../../customHooks/useWindowResizeChange';
-import Label from '@bit/redsky.framework.rs.label/dist/Label';
+import Label from '@bit/redsky.framework.rs.label';
+import Paper from '../../components/paper/Paper';
+import LabelInput from '../../components/labelInput/LabelInput';
+import LabelButton from '../../components/labelButton/LabelButton';
+import LabelLink from '../../components/labelLink/LabelLink';
 
-const SignupPage: React.FC = () => {
-	const size = useWindowResizeChange();
+interface SignUpPageProps {}
 
+const SignUpPage: React.FC<SignUpPageProps> = (props) => {
 	return (
-		<Page className={'rsSignupPage'}>
+		<Page className={'rsSignUpPage'}>
 			<div className={'rs-page-content-wrapper'}>
-				<Box
-					className={'heroImgAndText'}
-					display={'flex'}
-					alignItems={'center'}
-					marginBottom={size === 'small' ? 366 : 135}
-				>
-					<Box className={'signupTitleBox'} marginTop={119} marginLeft={140}>
-						<Label className={'signupTitleLabel'}>Sign up for Spire Loyalty</Label>
+				<Box display={'flex'} margin={'100px 0'} justifyContent={'center'}>
+					<Box maxWidth={'480px'} marginRight={38}>
+						<Label variant={'h1'}>Sign up for spire Loyalty</Label>
+						<Label variant={'body1'}>
+							Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+							invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+							et justo duo dolores et
+						</Label>
 					</Box>
-					<Box className={'formWrapperBox'} />
+					<Box marginLeft={38}>
+						<Paper
+							width={'604px'}
+							height={'844px'}
+							boxShadow
+							backgroundColor={'#FCFBF8'}
+							position={'relative'}
+						>
+							<Box padding={'48px 92px'}>
+								<Box display={'flex'} justifyContent={'space-between'}>
+									<LabelInput
+										title={'Name *'}
+										placeholder={'First and Last'}
+										onChange={(value) => console.log(value)}
+										inputType={'text'}
+									/>
+									<LabelInput
+										title={'Date Of Birth *'}
+										placeholder={'MM/DD/YYYY'}
+										onChange={(value) => console.log(value)}
+										inputType={'text'}
+									/>
+								</Box>
+								<LabelInput
+									title={'Address *'}
+									placeholder={'Street, Apartment or Suite'}
+									onChange={(value) => console.log(value)}
+									inputType={'text'}
+								/>
+								<Box display={'flex'} justifyContent={'space-between'}>
+									<LabelInput
+										title={'City *'}
+										placeholder={'City'}
+										onChange={(value) => console.log(value)}
+										inputType={'text'}
+									/>
+									<LabelInput
+										title={'Zip Code *'}
+										placeholder={'Zip Code'}
+										onChange={(value) => console.log(value)}
+										inputType={'text'}
+									/>
+								</Box>
+								<Box display={'flex'} justifyContent={'space-between'}>
+									<LabelInput
+										title={'Phone *'}
+										placeholder={'(  )  - '}
+										onChange={(value) => console.log(value)}
+										inputType={'tel'}
+										isPhoneInput
+									/>
+									<LabelInput
+										title={'Email *'}
+										placeholder={'email@address.com'}
+										onChange={(value) => console.log(value)}
+										inputType={'text'}
+										isEmailInput
+									/>
+								</Box>
+								<LabelInput
+									title={'Password *'}
+									placeholder={'Minimum 8 characters'}
+									onChange={(value) => console.log(value)}
+									inputType={'text'}
+								/>
+								<LabelInput
+									title={'Confirm Password *'}
+									placeholder={'Re-type password to confirm'}
+									onChange={(value) => console.log(value)}
+									inputType={'text'}
+								/>
+
+								<Box display={'flex'} flexDirection={'column'}>
+									<label className={'checkboxContainer'}>
+										<input
+											value={'Sign Up for newsletter'}
+											type={'checkbox'}
+											className={'checkboxInput'}
+											onClick={(e) => {
+												let inputValue = e.target as HTMLInputElement;
+												if (inputValue.checked) console.log(inputValue.value);
+											}}
+										/>
+										<span className={'checkbox'}>
+											<Box />
+										</span>
+										Sign up for newsletter
+									</label>
+									<label className={'checkboxContainer'}>
+										<input
+											value={'Sign Up for email notifications'}
+											type={'checkbox'}
+											className={'checkboxInput'}
+											onClick={(e) => {
+												let inputValue = e.target as HTMLInputElement;
+												if (inputValue.checked) console.log(inputValue.value);
+											}}
+										/>
+										<span className={'checkbox'}>
+											<Box />
+										</span>
+										Allow Spire to send email notifications
+									</label>
+								</Box>
+
+								<LabelButton
+									look={'containedPrimary'}
+									variant={'button'}
+									label={'Sign Up'}
+									onClick={() => console.log('Submit...')}
+								/>
+								<div className={'termsAndPrivacy'}>
+									By signing up you are agreeing to the <a href={'/'}>Terms & Conditions</a> and{' '}
+									<a href={'/'}>Privacy Policy.</a>
+								</div>
+							</Box>
+							<Box
+								className={'signInBottomBox'}
+								display={'flex'}
+								flexDirection={'column'}
+								justifyContent={'center'}
+								alignItems={'center'}
+								marginTop={'auto'}
+								height={'100px'}
+								borderTop={'1px solid #DEDEDE'}
+							>
+								<Label variant={'body1'}>Already have an account?</Label>
+								<LabelLink path={'/signin'} externalLink={false} label={'Sign In'} variant={'button'} />
+							</Box>
+						</Paper>
+					</Box>
 				</Box>
+				<Footer links={FooterLinkTestData} />
 			</div>
 		</Page>
 	);
 };
 
-export default SignupPage;
+export default SignUpPage;
