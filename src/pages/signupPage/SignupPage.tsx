37dfb824acfc33918458c6e384b4297dc156bb0f
janel <janell@redskytech.io>
2021-02-16 13:51:01 -0700

    file name change to match gitlab

--- src/pages/signupPage/SignupPage.tsx
+++ src/pages/signupPage/SignupPage.tsx
@@ -33,7 +33,7 @@ const SignUpPage: React.FC<SignUpPageProps> = (props) => {
 						<Label variant={'body1'}>
 							Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
 							invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-							et justo duo dolores et
+							et justo duo dolores et.
 						</Label>
 					</Box>
 					<Box marginLeft={38}>
