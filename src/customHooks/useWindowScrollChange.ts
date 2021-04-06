7fad1a467582eafec3d5d1f8a0543c4ebdd22adb
philipdokic <hey@phildjokic.com>
2021-04-05 18:10:54 -0600

    Hide App bar on down scroll.

---
+++ src/customHooks/useWindowScrollChange.ts
@@ -0,0 +1,29 @@
+/**
+ * This hook will return either "UP" or "DOWN" depending on which direction you are scrolling.
+ */
+
+import { useState } from 'react';
+import debounce from 'lodash.debounce';
+
+const SCROLL_FROM_TOP_BUFFER = 80;
+
+export default function useWindowScrollChange() {
+	const [screenScrollDirection, setScreenScrollDirection] = useState<'UP' | 'DOWN'>();
+
+	let prevScrollPosition = window.pageYOffset;
+
+	let scrollDebounce = debounce(async () => {
+		let currentScrollPos = window.pageYOffset;
+		if (prevScrollPosition > currentScrollPos) {
+			setScreenScrollDirection('UP');
+		} else if (currentScrollPos < SCROLL_FROM_TOP_BUFFER) {
+			setScreenScrollDirection('UP');
+		} else {
+			setScreenScrollDirection('DOWN');
+		}
+		prevScrollPosition = currentScrollPos;
+	}, 10);
+
+	window.addEventListener('scroll', scrollDebounce);
+	return screenScrollDirection;
+}
