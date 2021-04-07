62eae4b961a28532c5727987bf4de8e12a6c9ef1
janel <janell@redskytech.io>
2021-04-07 14:07:45 -0600

    fixed filter bar to be reusable and reservation page to work with new filter bar

--- src/pages/redeemableRewardsPage/RedeemableRewardsPage.tsx
+++ src/pages/redeemableRewardsPage/RedeemableRewardsPage.tsx
@@ -3,7 +3,11 @@ import './RedeemableRewardsPage.scss';
 import { Page } from '@bit/redsky.framework.rs.996';
 
 const RedeemableRewardsPage: React.FC = () => {
-	return <Page className={'rsRedeemableRewards'}></Page>;
+	return (
+		<Page className={'rsRedeemableRewardsPage'}>
+			<div className={'rs-page-content-wrapper'}></div>
+		</Page>
+	);
 };
 
 export default RedeemableRewardsPage;
