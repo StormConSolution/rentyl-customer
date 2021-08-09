8d19db96258bff65dd209cb559c0909cb3fcef72
thomas <thomas.crockett@redskytech.io>
2021-08-09 13:51:49 -0600

    Added better check for a reward existing.

--- src/pages/orderConfirmationPage/OrderConfirmationPage.tsx
+++ src/pages/orderConfirmationPage/OrderConfirmationPage.tsx
@@ -10,6 +10,7 @@ import { FooterLinkTestData } from '../../components/footer/FooterLinks';
 import Box from '../../components/box/Box';
 import Label from '@bit/redsky.framework.rs.label/dist/Label';
 import { DateUtils } from '../../utils/utils';
+import LoadingPage from '../loadingPage/LoadingPage';
 
 const OrderConfirmationPage = () => {
 	const rewardService = serviceFactory.get<RewardService>('RewardService');
@@ -21,7 +22,7 @@ const OrderConfirmationPage = () => {
 
 	useEffect(() => {
 		async function getRewardDetails() {
-			if (!params.reward) {
+			if (!params.reward || !params.voucherCode) {
 				router.navigate('/reward').catch(console.error);
 			}
 			try {
@@ -29,6 +30,7 @@ const OrderConfirmationPage = () => {
 				setReward(res);
 			} catch (e) {
 				rsToasts.error('Reward no longer exists.');
+				router.navigate('/').catch(console.error);
 			}
 		}
 		getRewardDetails().catch(console.error);
@@ -44,7 +46,9 @@ const OrderConfirmationPage = () => {
 		return styles;
 	}
 
-	return (
+	return !reward ? (
+		<LoadingPage />
+	) : (
 		<Page className={'rsOrderConfirmationPage'}>
 			<div className={'rs-page-content-wrapper'}>
 				<Box display={'flex'} justifyContent={'space-between'} className={'confirmationHeader'}>
@@ -65,13 +69,13 @@ const OrderConfirmationPage = () => {
 					<Box display={'flex'}>
 						<div style={renderStyle()} className={'rewardImage'} />
 						<Box display={'flex'} flexDirection={'column'} justifyContent={'space-between'} margin={20}>
-							<Label variant={'body1'}>{reward?.description}</Label>
-							<Label variant={'h3'}>Item #{reward?.upc}</Label>
+							<Label variant={'body1'}>{reward.description}</Label>
+							<Label variant={'h3'}>Item #{reward.upc}</Label>
 							<Label variant={'h3'}>Gift Certificate # {params.voucherCode}</Label>
 							<Label variant={'caption'}>Redemption Instructions</Label>
 						</Box>
 					</Box>
-					<Label variant={'h3'}>{reward?.pointCost} Points</Label>
+					<Label variant={'h3'}>{reward.pointCost} Points</Label>
 				</Box>
 				<Footer links={FooterLinkTestData} />
 			</div>
