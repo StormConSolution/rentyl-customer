4ac30e9054ddca5503293365d417a3d30b6ce4fb
philipdokic <hey@phildjokic.com>
2021-08-11 11:52:23 -0600

    - Created an EditExistingServicePage to allow you to edit your current reservations services. - Updated the look of the DestinationPackageTile - Removed the Model for Reservations because I changed how we use the getByPaged,

--- src/customHooks/useWindowResizeChange.ts
+++ src/customHooks/useWindowResizeChange.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const DefaultBreakpoints = {};
-
+//Temp fix below will change after mobile vs. desktop fix.
 // const MobileAndDown = 1160;
 const MobileAndDown = 640;
 
