afc59058c27565363f6b95a88404f3de9d210770
thomas <thomas.crockett@redskytech.io>
2021-11-19 10:20:42 -0700

    Added load more for pagination instead of buttons, changed reservation availability and destination details pages to work with a load more instead of the pagination.

--- src/components/labelLink/LabelLink.tsx
+++ src/components/labelLink/LabelLink.tsx
@@ -8,22 +8,7 @@ interface LabelLinkProps {
 	path: string;
 	externalLink?: boolean;
 	label: string;
-	variant:
-		| 'h1'
-		| 'h2'
-		| 'h3'
-		| 'h4'
-		| 'h5'
-		| 'h6'
-		| 'subtitle1'
-		| 'subtitle2'
-		| 'body1'
-		| 'body2'
-		| 'caption'
-		| 'button'
-		| 'overline'
-		| 'srOnly'
-		| 'inherit';
+	variant: Misc.Variant;
 	iconLeft?: string;
 	iconRight?: string;
 	iconSize?: number;
