c013733a20744435d53bd39aee17ecdc2444818a
Thomas Crockett <thomas.crockett24@gmail.com>
2021-11-20 10:28:56 -0700

    remove loading text on initial render

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,6 @@ function App() {
 	}, [loginStatus, isCompanyLoaded]);
 
 	function renderViewsBasedOnLoginStatus() {
-		if (!isCompanyLoaded) return <>Loading...</>; //TODO: need to adjust for spire vs rental
 		switch (loginStatus) {
 			case LoginStatus.UNKNOWN:
 				return null;
