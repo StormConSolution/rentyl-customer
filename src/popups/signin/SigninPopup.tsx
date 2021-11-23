c4c758d17ee3edfc374b98f519af149b7db46e03
Alex Lauritzen <alex.lauritzen@redskytech.io>
2021-11-23 16:01:57 -0700

    Ticket 2901: Alters the sign in sign up modals to be submitted by pressing enter

--- src/popups/signin/SigninPopup.tsx
+++ src/popups/signin/SigninPopup.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './SigninPopup.scss';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { Box, Popup, popupController } from '@bit/redsky.framework.rs.996';
 import Label from '@bit/redsky.framework.rs.label/dist/Label';
 import Paper from '../../components/paper/Paper';
@@ -8,7 +8,6 @@ import Icon from '@bit/redsky.framework.rs.icon';
 import { RsFormControl, RsFormGroup, RsValidator, RsValidatorEnum } from '@bit/redsky.framework.rs.form';
 import { PopupProps } from '@bit/redsky.framework.rs.996/dist/popup/Popup';
 import LabelInput from '../../components/labelInput/LabelInput';
-import Button from '@bit/redsky.framework.rs.button';
 import serviceFactory from '../../services/serviceFactory';
 import UserService from '../../services/user/user.service';
 import SignupPopup, { SignupPopupProps } from '../signup/SignupPopup';
@@ -36,7 +35,8 @@ const SigninPopup: React.FC<SigninPopupProps> = (props) => {
 		}
 	}
 
-	async function signin() {
+	async function signIn(event?: FormEvent<HTMLFormElement>) {
+		if (event) event.preventDefault();
 		if (!(await signinForm.isValid())) {
 			setSigninForm(signinForm.clone());
 			setErrorMessage('Missing information');
@@ -57,31 +57,33 @@ const SigninPopup: React.FC<SigninPopupProps> = (props) => {
 	return (
 		<Popup opened={props.opened} preventCloseByBackgroundClick>
 			<Paper className={'rsSigninPopup'} position={'relative'}>
-				<Icon
-					iconImg={'icon-close'}
-					onClick={() => {
-						popupController.close(SigninPopup);
-					}}
-					size={20}
-					cursorPointer
-				/>
-				<Label variant={'h4'}>Sign in / Sign up</Label>
-				<hr className={'lineThrough'} />
-				<LabelInput
-					title={'Email Address'}
-					inputType={'email'}
-					control={signinForm.get('primaryEmail')}
-					updateControl={updateForm}
-					labelVariant={'body5'}
-				/>
-				<LabelInput
-					title={'Password'}
-					inputType={'password'}
-					control={signinForm.get('password')}
-					updateControl={updateForm}
-					labelVariant={'body5'}
-				/>
-				<LabelButton look={'containedPrimary'} onClick={signin} label={'Sign in'} variant={'button'} />
+				<form onSubmit={signIn}>
+					<Icon
+						iconImg={'icon-close'}
+						onClick={() => {
+							popupController.close(SigninPopup);
+						}}
+						size={20}
+						cursorPointer
+					/>
+					<Label variant={'h4'}>Sign in / Sign up</Label>
+					<hr className={'lineThrough'} />
+					<LabelInput
+						title={'Email Address'}
+						inputType={'email'}
+						control={signinForm.get('primaryEmail')}
+						updateControl={updateForm}
+						labelVariant={'body5'}
+					/>
+					<LabelInput
+						title={'Password'}
+						inputType={'password'}
+						control={signinForm.get('password')}
+						updateControl={updateForm}
+						labelVariant={'body5'}
+					/>
+					<LabelButton look={'containedPrimary'} label={'Sign in'} variant={'button'} buttonType={'submit'} />
+				</form>
 				<Label className={'errorMessage'} variant={'body2'} color={'red'}>
 					{errorMessage}
 				</Label>
