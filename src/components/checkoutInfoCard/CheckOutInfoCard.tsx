ba38aae823f79edb5aae42a4da90f2fd3731e221
Thomas Crockett <thomas.crockett24@gmail.com>
2021-11-23 23:43:16 -0700

    added a scss file and connected all the fields to a form control

--- src/components/checkoutInfoCard/CheckOutInfoCard.tsx
+++ src/components/checkoutInfoCard/CheckOutInfoCard.tsx
@@ -1,29 +1,88 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import './CheckOutInfoCard.scss';
 import { Box } from '@bit/redsky.framework.rs.996';
 import Label from '@bit/redsky.framework.rs.label/dist/Label';
-import Input from '@bit/redsky.framework.rs.input';
 import { RsFormControl, RsFormGroup, RsValidator, RsValidatorEnum } from '@bit/redsky.framework.rs.form';
 import LabelInput from '../labelInput/LabelInput';
 import LabelSelect from '../labelSelect/LabelSelect';
+import { useRecoilValue } from 'recoil';
+import globalState from '../../state/globalState';
+import { rsToastify } from '@bit/redsky.framework.rs.toastify';
+import { WebUtils } from '../../utils/utils';
+import serviceFactory from '../../services/serviceFactory';
+import CountryService from '../../services/country/country.service';
+import Button from '@bit/redsky.framework.rs.button';
+import LabelButton from '../labelButton/LabelButton';
 
 export interface CheckOutInfoCardProps {}
 const CheckOutInfoCard: React.FC<CheckOutInfoCardProps> = (props) => {
+	const user = useRecoilValue<Api.User.Res.Get | undefined>(globalState.user);
+	const countryService = serviceFactory.get<CountryService>('CountryService');
+	const [stateList, setStateList] = useState<Misc.OptionType[]>([]);
+	const [countryList, setCountryList] = useState<Misc.OptionType[]>([]);
 	const [infoForm, setInfoForm] = useState<RsFormGroup>(
 		new RsFormGroup([
-			new RsFormControl('firstName', '', [new RsValidator(RsValidatorEnum.REQ, 'Enter a first name')]),
-			new RsFormControl('lastName', '', [new RsValidator(RsValidatorEnum.REQ, 'Enter a last name')]),
-			new RsFormControl('address1', '', [new RsValidator(RsValidatorEnum.REQ, 'Enter a password')]),
-			new RsFormControl('address2', '', []),
-			new RsFormControl('zip', '', [new RsValidator(RsValidatorEnum.REQ, 'Enter a zip code')]),
-			new RsFormControl('city', '', [new RsValidator(RsValidatorEnum.REQ, 'Enter your city')]),
-			new RsFormControl('state', 0, []),
-			new RsFormControl('primaryEmailAddress', '', [
-				new RsValidator(RsValidatorEnum.EMAIL, 'Enter a valid email')
+			new RsFormControl('firstName', user?.firstName || '', [
+				new RsValidator(RsValidatorEnum.REQ, 'First name is required')
 			]),
-			new RsFormControl('phone', '', [new RsValidator(RsValidatorEnum.REQ, 'Enter a phone number')])
+			new RsFormControl('lastName', user?.lastName || '', [
+				new RsValidator(RsValidatorEnum.REQ, 'Last name is required')
+			]),
+			new RsFormControl('email', user?.primaryEmail || '', [
+				new RsValidator(RsValidatorEnum.EMAIL, 'Enter a valid Email')
+			]),
+			new RsFormControl('phone', user?.phone || '', [
+				new RsValidator(RsValidatorEnum.REQ, 'A phone number is required')
+			]),
+			new RsFormControl('address1', '', [new RsValidator(RsValidatorEnum.REQ, 'Address is required')]),
+			new RsFormControl('address2', '', []),
+			new RsFormControl('city', '', [new RsValidator(RsValidatorEnum.REQ, 'City is required')]),
+			new RsFormControl('zip', '', [new RsValidator(RsValidatorEnum.REQ, 'Zip is required')]),
+			new RsFormControl('state', '', [new RsValidator(RsValidatorEnum.REQ, 'State is required')]),
+			new RsFormControl('country', 'US', [new RsValidator(RsValidatorEnum.REQ, 'Country is required')])
 		])
 	);
+
+	useEffect(() => {
+		async function getCountries() {
+			try {
+				let countries = await countryService.getAllCountries();
+				setCountryList(formatStateOrCountryListForSelect(countries.countries));
+			} catch (e) {
+				rsToastify.error(WebUtils.getRsErrorMessage(e, 'Unable to get a list of countries.'), 'Server Error');
+			}
+		}
+		getCountries().catch(console.error);
+	}, []);
+
+	useEffect(() => {
+		async function getStates() {
+			let selectedCountry = infoForm.get('country');
+			if (!selectedCountry) return;
+			try {
+				let response = await countryService.getStates(`${selectedCountry.value}`);
+				if (response.states) {
+					setStateList(formatStateOrCountryListForSelect(response.states));
+				}
+				let stateValue = infoForm.get('state');
+				stateValue.value = '';
+				setInfoForm(infoForm.clone().update(stateValue));
+			} catch (e) {
+				rsToastify.error(
+					WebUtils.getRsErrorMessage(e, 'Unable to get states for the selected country.'),
+					'Server Error'
+				);
+			}
+		}
+		getStates().catch(console.error);
+	}, [infoForm.get('country').value]);
+
+	function formatStateOrCountryListForSelect(statesOrCountries: any[]) {
+		return statesOrCountries.map((item) => {
+			return { value: item.isoCode, label: item.name };
+		});
+	}
 
 	function updateForm(control: RsFormControl) {
 		setInfoForm(infoForm.clone().update(control));
@@ -39,33 +98,46 @@ const CheckOutInfoCard: React.FC<CheckOutInfoCardProps> = (props) => {
 			</Box>
 			<form>
 				<LabelInput
-					title={'First name'}
+					title={'First Name'}
 					inputType={'text'}
 					control={infoForm.get('firstName')}
 					updateControl={updateForm}
 				/>
 				<LabelInput
-					title={'Last name'}
+					title={'Last Name'}
 					inputType={'text'}
 					control={infoForm.get('lastName')}
 					updateControl={updateForm}
 				/>
 				<LabelInput
-					title={'Address line 1'}
+					title={'Email'}
+					inputType={'text'}
+					control={infoForm.get('email')}
+					updateControl={updateForm}
+				/>
+				<LabelInput
+					inputType={'tel'}
+					title={'Phone'}
+					isPhoneInput
+					onChange={(value) => {
+						let updatedPhone = infoForm.getClone('phone');
+						updatedPhone.value = value;
+						updateForm(updatedPhone);
+					}}
+					initialValue={user?.phone}
+				/>
+				<LabelInput
+					className={'stretchedInput'}
+					title={'Address Line 1'}
 					inputType={'text'}
 					control={infoForm.get('address1')}
 					updateControl={updateForm}
 				/>
 				<LabelInput
-					title={'Address line 2'}
+					className={'stretchedInput'}
+					title={'Address Line 2'}
 					inputType={'text'}
 					control={infoForm.get('address2')}
-					updateControl={updateForm}
-				/>
-				<LabelInput
-					title={'Post/Zip code'}
-					inputType={'text'}
-					control={infoForm.get('zip')}
 					updateControl={updateForm}
 				/>
 				<LabelInput
@@ -74,19 +146,20 @@ const CheckOutInfoCard: React.FC<CheckOutInfoCardProps> = (props) => {
 					control={infoForm.get('city')}
 					updateControl={updateForm}
 				/>
-				<LabelSelect title={'State'} control={infoForm.get('state')} options={} updateControl={updateForm} />
-				<LabelInput
-					title={'primaryEmailAddress'}
-					inputType={'email'}
-					control={infoForm.get('firstName')}
+				<LabelSelect
+					title={'State'}
+					options={stateList}
+					control={infoForm.get('state')}
 					updateControl={updateForm}
 				/>
-				<LabelInput
-					title={'Phone'}
-					inputType={'tel'}
-					control={infoForm.get('phone')}
+				<LabelInput title={'Zip'} inputType={'text'} control={infoForm.get('zip')} updateControl={updateForm} />
+				<LabelSelect
+					title={'Country'}
+					options={countryList}
+					control={infoForm.get('country')}
 					updateControl={updateForm}
 				/>
+				<LabelButton look={'containedPrimary'} variant={'body1'} label={'Continue'} onClick={() => {}} />
 			</form>
 		</Box>
 	);
--- src/pages/testPage/TestPage.tsx
+++ src/pages/testPage/TestPage.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import { Box } from '@bit/redsky.framework.rs.996';
+import CheckOutInfoCard from '../../components/checkoutInfoCard/CheckOutInfoCard';
 
 const TestPage: React.FC = () => {
-	return <Box></Box>;
+	return (
+		<Box mt={72}>
+			<CheckOutInfoCard />
+		</Box>
+	);
 };
 
 export default TestPage;
