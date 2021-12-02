569c22e93b4492f572456280ae365bb2af2a0aa8
@janell1 <janell@redskytech.io>
2021-12-02 10:26:54 -0700

    Main Drop Down Menu re-design.

--- src/popups/navDrawer/NavDrawer.tsx
+++ src/popups/navDrawer/NavDrawer.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import './NavDrawer.scss';
-import Icon from '@bit/redsky.framework.rs.icon';
 import { useEffect, useRef } from 'react';
 import { Box, popupController } from '@bit/redsky.framework.rs.996';
 import LabelLink from '../../components/labelLink/LabelLink';
@@ -9,10 +8,9 @@ import serviceFactory from '../../services/serviceFactory';
 import UserService from '../../services/user/user.service';
 import { useRecoilValue } from 'recoil';
 import globalState from '../../state/globalState';
-import { isRouteUnauthorized } from '../../utils/utils';
-import { NavData } from './NavData';
 import SigninPopup, { SigninPopupProps } from '../signin/SigninPopup';
 import SignupPopup, { SignupPopupProps } from '../signup/SignupPopup';
+import Label from '@bit/redsky.framework.rs.label';
 
 interface NavPopoutProps {
 	onClose: () => void;
@@ -37,88 +35,87 @@ const NavDrawer: React.FC<NavPopoutProps> = (props) => {
 		};
 	}, []);
 
-	function renderNavLinks(forUser: boolean) {
-		return NavData.filter((item) => {
-			return forUser === item.isSignedIn;
-		}).map((item, index) => {
-			if (isRouteUnauthorized(item.route)) return false;
-
-			return (
-				<LabelLink
-					key={index}
-					path={item.route}
-					label={item.title}
-					variant={item.isSectionHeader ? 'h2' : 'h4'}
-					onClick={() => {
-						props.onClose();
-						router.navigate(item.route).catch(console.error);
-					}}
-				/>
-			);
-		});
-	}
-
 	return (
 		<>
 			<div ref={popupRef} className={props.isOpened ? `rsNavDrawer opened` : 'rsNavDrawer'}>
-				<Icon iconImg={'icon-close'} onClick={props.onClose} size={21} color={'#ffffff'} cursorPointer />
-				<Box mt={100} ml={40}>
-					<Box mb={30}>{renderNavLinks(false)}</Box>
-					{user && renderNavLinks(true)}
-				</Box>
-				<Box
-					display={'flex'}
-					alignItems={'center'}
-					justifyContent={'space-evenly'}
-					borderTop={'1px solid #fff'}
-					position={'absolute'}
-					bottom={0}
-					left={0}
-					height={62}
-					width={'100%'}
-				>
+				<Box padding={'24px 24px 10px'}>
 					{!user ? (
 						<>
 							<LabelLink
 								path={'/signup'}
 								label={'Sign Up'}
-								variant={'button'}
-								iconRight={'icon-chevron-right'}
+								variant={'navDrawerCustomOne'}
 								onClick={() => {
 									props.onClose();
 									popupController.open<SignupPopupProps>(SignupPopup, {});
 								}}
-								iconSize={7}
-								iconColor={'#ffffff'}
 							/>
 							<LabelLink
 								path={'/signin'}
 								label={'Log in'}
-								variant={'button'}
-								iconRight={'icon-chevron-right'}
+								variant={'navDrawerCustomOne'}
 								onClick={() => {
 									props.onClose();
 									popupController.open<SigninPopupProps>(SigninPopup, {});
 								}}
-								iconSize={7}
-								iconColor={'#ffffff'}
 							/>
 						</>
 					) : (
-						<LabelLink
-							path={'/'}
-							label={'Log out'}
-							variant={'button'}
-							iconRight={'icon-chevron-right'}
-							iconSize={7}
-							iconColor={'#ffffff'}
-							onClick={() => {
-								userService.logout();
-								props.onClose();
-								router.navigate('/');
-							}}
-						/>
+						<>
+							<Label className={'welcomeLabel'} variant={'navDrawerCustomTwo'}>
+								Welcome, {user.firstName} {user.lastName}
+							</Label>
+							<LabelLink
+								path={'/'}
+								label={'My Account'}
+								variant={'navDrawerCustomOne'}
+								onClick={() => {
+									props.onClose();
+									router.navigate('/account/personal-info');
+								}}
+							/>
+							<LabelLink
+								path={'/'}
+								label={'Reservations'}
+								variant={'navDrawerCustomOne'}
+								onClick={() => {
+									props.onClose();
+									router.navigate('/reservations');
+								}}
+							/>
+
+							<LabelLink
+								path={'/'}
+								label={'Log out'}
+								variant={'navDrawerCustomOne'}
+								onClick={() => {
+									userService.logout();
+									props.onClose();
+									router.navigate('/');
+								}}
+							/>
+						</>
 					)}
+				</Box>
+				<hr />
+				<Box padding={'24px 24px 10px'}>
+					<LabelLink
+						path={'/contact'}
+						label={'Contact Us'}
+						variant={'navDrawerCustomOne'}
+						onClick={() => {
+							window.open('https://rentylresorts.com/contact-us/', '_blank');
+						}}
+					/>
+					<LabelLink
+						path={'/'}
+						label={'Help'}
+						variant={'navDrawerCustomOne'}
+						onClick={() => {
+							props.onClose();
+							// popupController.open<SignupPopupProps>(SignupPopup, {});
+						}}
+					/>
 				</Box>
 			</div>
 			<div className={`transparentOverlay ${props.isOpened ? 'opened' : ''}`} />
