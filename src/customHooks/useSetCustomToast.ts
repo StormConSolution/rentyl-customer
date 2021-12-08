9f876a10dc52e163f69be90058ebd3b020cbbd75
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-12-08 08:50:44 -0700

    removing unneeded components

--- src/customHooks/useSetCustomToast.ts
+++
@@ -1,9 +0,0 @@
-import rsToasts from '@bit/redsky.framework.toast';
-import CustomToast from '../components/customToast/CustomToast';
-import { useEffect } from 'react';
-
-export function useSetCustomToast() {
-	useEffect(() => {
-		rsToasts.setRenderDelegate(CustomToast);
-	}, []);
-}
