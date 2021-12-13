906442d91b114bf73f2bf0df06e0bf3332ae6683
Landon <landon@redskytech.io>
2021-12-13 14:22:40 -0700

    Refactor: Added size custom hook to footer to change font variations to list headers Task: Task 3606

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,6 +9,7 @@ import { useRecoilValue } from 'recoil';
 import globalState from '../../state/globalState';
 import { isRouteUnauthorized } from '../../utils/utils';
 import SignupBanner from '../signupBanner/SignupBanner';
+import useWindowResizeChange from '../../customHooks/useWindowResizeChange';
 
 interface FooterLink {
 	text: string;
@@ -27,6 +28,7 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = (props) => {
 	const company = useRecoilValue<Api.Company.Res.GetCompanyAndClientVariables>(globalState.company);
 	const user = useRecoilValue<Api.User.Res.Detail | undefined>(globalState.user);
+	const size = useWindowResizeChange();
 	useEffect(() => {
 		let id = router.subscribeToBeforeRouterNavigate(() => {
 			setTimeout(() => {
@@ -59,7 +61,7 @@ const Footer: React.FC<FooterProps> = (props) => {
 		return sections.map((section: FooterSection, index) => {
 			return (
 				<Box className={'footerSection'} key={index} display={'flex'} flexDirection={'column'}>
-					<Label variant={'reservationDetailsCustomOne'} mb={7}>
+					<Label variant={size === 'small' ? 'reservationDetailsCustomOne' : 'h4'} mb={7}>
 						{section.title}
 					</Label>
 					{renderLinks(section.links)}
